2d720798a556e2b141a19a23be8a088da2eac154
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-01-21 20:14:47 -0300

    GoStack - Nível 01 - Back-end com Node.js - Aplicação Funcional

--- nivel01/backend/src/index.js
+++ nivel01/backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -22,34 +23,60 @@ app.use(express.json());
  * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
  */
 
+/**
+ * Middleware:
+ *
+ * Interceptador de requisições que interromper totalmente a requisiçao ou alterar dados da requisição.
+ */
+
+const projects = [];
+
 app.get('/projects', (req, res) => {
-  const { title, owner } = req.query;
-
-  console.log(title);
-  console.log(owner);
-
-  return res.json(['Projeto 1', 'Projeto 2']);
+  return res.json(results);
 });
 
 app.post('/projects', (req, res) => {
-  const {title, owner} = req.body;
+  const { title, owner } = req.body;
 
-  console.log(title);
-  console.log(owner);
+  const project = { id: uuid(), title, owner };
+  projects.push(project);
 
-  return res.json(['Projeto 1', 'Projeto 2', 'Projeto 3']);
+  return res.json(project);
 });
 
 app.put('/projects/:id', (req, res) => {
   const { id } = req.params;
+  const { title, owner } = req.body;
 
-  console.log(id);
+  const projectIndex = projects.findIndex((project) => project.id === id);
 
-  return res.json(['Projeto 4', 'Projeto 2', 'Projeto 3']);
+  if (projectIndex < 0) {
+    return res.status(400).json({ error: 'Project not found.' });
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return res.json(project);
 });
 
 app.delete('/projects/:id', (req, res) => {
-  return res.json(['Projeto 2', 'Projeto 3']);
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return res.status(400).json({ error: 'Project not found.' });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return res.status(204).send();
 });
 
 app.listen(3333, () => {
