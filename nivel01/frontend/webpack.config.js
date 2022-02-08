c442070032d298752d3e40fa79d0a7ab681b2435
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-02-08 20:22:05 -0300

    GoStack - Nível 01 - Front-end com ReactJS - Componentização

--- nivel01/frontend/webpack.config.js
+++ nivel01/frontend/webpack.config.js
@@ -5,11 +5,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
-    publicPath: 'public/'
   },
-  devServer: {
-    static: path.join(__dirname, "public"),
-  },
+  mode: 'development',
   module: {
     rules: [
       {
