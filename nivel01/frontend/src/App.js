74b9c9738c9024abdf65873c99f78a11df2804b9
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-02-09 20:37:00 -0300

    GoStack - Nível 01 - Front-end com ReactJS - Estado e Imutabilidade

--- nivel01/frontend/src/App.js
+++ nivel01/frontend/src/App.js
@@ -1,29 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Header";
 
 /**
  * Componente
  * Propriedade
- * Estado
+ * Estado & Imutabilidade
  */
 
 function App() {
+  const [projects, setProjects] = useState([
+    "Desenvolvimento de app",
+    "Front-end web",
+  ]);
+
+  // useState retorna um array com 2 posições
+  // 1. Variável com o seu valor inicial
+  // 2. Função para atualizarmos esse valor
+
+  function handleAddProject() {
+    // projects.push(`Novo Projeto ${Date.now()}`);
+
+    // Imutabilidade -> copia as informações do array e adiciona as novas informações
+    // spread operator ([...array], {...object}) -> percorre o elemento e copia cada posição/chave do elemento
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+
+    console.log(projects);
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Project">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title="Project" />
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
     </>
   );
 }
