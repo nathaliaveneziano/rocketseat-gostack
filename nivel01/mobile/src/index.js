472b2e88fa16da3eb6dba1179feaa9645b322710
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-02-17 20:52:10 -0300

    GoStack - Nível 01 - Mobile com React Native - Listando projetos da API

--- nivel01/mobile/src/index.js
+++ nivel01/mobile/src/index.js
@@ -1,20 +1,45 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+} from 'react-native';
 
-// Não possuem valor semântico (significado)
-// Não possuem estilização própria
-// Todos componentes possuem por padrão "display: flex"
-
-// View (container) -> div, footer, header, main, aside, section, entre outros
-// Text -> p, span, strong, h1, h2, h3
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
   return (
     <>
-      <StatusBar barStyle='light-content' backgroundColor={'#7159c1'} />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+      <StatusBar barStyle="light-content" backgroundColor={'#7159c1'} />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({item: project}) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
+
+      {/* <View style={styles.container}>
+        {projects.map(project => (
+          <Text key={project.id} style={styles.project}>
+            {project.title}
+          </Text>
+        ))}
+      </View> */}
     </>
   );
 }
@@ -23,12 +48,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  title: {
+  project: {
     color: '#fff',
-    fontSize: 32,
-    fontWeight: 'bold',
+    fontSize: 30,
   },
 });
